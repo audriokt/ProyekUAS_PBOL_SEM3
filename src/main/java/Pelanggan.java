cb51cdfefeab3fc7459becc2fa08472d62cc2e93
audro <zerotwo8802@gmail.com>
2024-11-26 00:05:11 +0700

    make customer class

---
+++ src/main/java/Pelanggan.java
@@ -0,0 +1,49 @@
+public class Pelanggan {
+    private String customerId;
+    private String customerName;
+    private String customerAddress;
+    private String customerPhone;
+    private String customerEmail;
+
+    public Pelanggan(String customerId, String customerName, String customerAddress, String customerPhone, String customerEmail) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerPhone = customerPhone;
+        this.customerEmail = customerEmail;
+    }
+    public String getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+    
+
+
+
+}
